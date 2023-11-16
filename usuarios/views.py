e3a5870c6e287ecba495e108fcde16295d437531
roge2 <rogerio.pereira@ifto.edu.br>
2023-11-15 21:57:36 -0300

    usuarios

---
+++ usuarios/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from .forms import UsuarioForm
+
+# Create your views here.
+def criar_usuario(request):
+    form = UsuarioForm(request.POST)
+    if request.method=="POST":
+        form = UsuarioForm(request.POST, request.FILES)
+        if  form.is_valid():
+            obj=form.save()
+            obj.save()
+            return redirect('login')
+    return render(request, 'usuarios/usuario.html',{'form':form})
